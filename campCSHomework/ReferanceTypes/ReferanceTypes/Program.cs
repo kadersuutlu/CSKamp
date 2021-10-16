ccbb174072dfb46569485aaf1ae1b944e2fb0e5d
kadersuutlu <ogr.k.sutlu34@ahievran.edu.tr>
2021-10-16 17:53:11 +0300

    Hafta4Ödev1

---
+++ campCSHomework/ReferanceTypes/ReferanceTypes/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ReferanceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int,decimal,float,enum,boolean => value types =>stack
+            int sayi1 = 10;
+            int sayi2 = 20;
+
+            sayi1 = sayi2;
+
+            sayi2 = 100;
+            Console.WriteLine(sayi1);
+
+            //array,class,interface => referance types =>stack+heap
+            int[] sayilar1 = new int[] { 1, 2, 3 };
+            int[] sayilar2 = new int[] { 10, 20, 30 };
+
+            sayilar1 = sayilar2;
+            sayilar2[0] = 1000;
+
+            Console.WriteLine(sayilar1[0]);
+
+            Person person1 = new Person();
+            Person person2 = new Person();
+            person1.FirstName = "Engin";
+
+            person2 = person1;
+            person1.FirstName = "Derin";
+
+            Console.WriteLine(person2.FirstName);
+
+            Customer customer = new Customer();
+            customer.FirstName = "Salih";
+            Employee employee = new Employee();
+
+            Person person3 = customer;
+            customer.FirstName = "Ahmet";
+            Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+            Console.WriteLine(person3.FirstName);
+
+            PersonManager personManager = new PersonManager();
+            personManager.Add(person3);
+            personManager.Add(customer);
+            personManager.Add(employee);
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+    }
+
+    //base class : Person (temel sınıf)
+    class Customer : Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Employee : Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(Person person) 
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
